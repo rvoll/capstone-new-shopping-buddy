a56209d8717e7796c6a261710c83dc7dafd1a0ef
Rebecca Voll <doktavoll@gmail.com>
2024-08-15 11:12:52 +0100

    small changes in index.js and in components folder

--- components/shoppingItem/shoppingItem.js
+++ components/shoppingItem/shoppingItem.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Link from "next/link";
 
-export default function ShoppingItem() {
+export default function ShoppingItem({ id, name }) {
   return (
     <main>
       <h1>Shopping Items</h1>
       <ul>
         {shoppingItems.map((shoppingItem) => (
-          <li key={shoppingItem.slug}>
+          <li key={shoppingItem.id}>
             {shoppingItem.quantity} {shoppingItem.name}, {shoppingItem.category}
             <Link href={`/shoppingItems/${shoppingItem.slug}`}>Details</Link>
           </li>
--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,14 @@ import { useRouter } from "next/router";
 export default function ShoppingItemsList() {
   const router = useRouter();
 
+  function ShoppingItem() {}
+
   return (
     <main>
       <h1> Shopping Items ({shoppingItems.length}) </h1>
       <ul>
         {shoppingItems.map((shoppingItem) => (
-          <li key={shoppingItem.slug}>
+          <li key={shoppingItem.id}>
             {shoppingItem.quantity} {shoppingItem.name}, {shoppingItem.category}
             <Link href={`/shoppingItems/${shoppingItem.slug}`}>Details</Link>
           </li>
