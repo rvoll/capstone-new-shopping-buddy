4351e4e8c5e79474a40e0983d596f8bd919171c4
Rebecca Voll <doktavoll@gmail.com>
2024-08-20 08:42:28 +0100

    remove comments from index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import { shoppingItems } from "../lib/shoppingItemsData.js";
 import { categories } from "../lib/categoriesData.js";
-// import { ShoppingItem } from "../components/shoppingItemhoppingItem";
 
 import Link from "next/link";
 import { useRouter } from "next/router";
@@ -11,21 +10,16 @@ export default function ShoppingItemsList() {
   return (
     <main>
       <StyledH1> {shoppingItems.length} Shopping Items </StyledH1>
-
       <StyledList>
         {shoppingItems.map((shoppingItem) => {
-          // Find the category object that matches the shoppingItem's category
           const category = categories.find(
             (category) => category.name === shoppingItem.category
           );
+          const backgroundColor = category ? category.color : "white";
 
-          // If the category exists, use its color, otherwise use a default color
-          const backgroundColor = category ? category.color : "red";
-
-          // Render the ListItem component
           return (
             <ListItem key={shoppingItem.id} backgroundColor={backgroundColor}>
-              {shoppingItem.quantity} {shoppingItem.name},{" "}
+              {shoppingItem.quantity} {shoppingItem.name}
               <CategoryBox>{shoppingItem.category}</CategoryBox>
               {/* <Link href={`/shoppingItems/${shoppingItem.slug}`}>Details</Link> */}
             </ListItem>
@@ -40,27 +34,26 @@ const StyledH1 = styled.h1`
   padding: 20px;
 `;
 
-const ListItem = styled.li`
-  background-color: ${(props) =>
-    props.backgroundColor}; /* Use the backgroundColor prop */
-  padding: 20px;
-  margin-bottom: 10px;
-  margin-right: 40px;
-  border-radius: 5px;
-  justify-content: center;
-`;
-
 const StyledList = styled.ul`
   list-style-type: none;
 `;
 
 const CategoryBox = styled.span`
-  display: inline-block;
-  padding: 5px 10px;
-  margin-left: 10px;
+  position: absolute;
+  top: 5px;
+  right: 5px;
+  padding: 2px 5px;
   background-color: #eee;
   color: #333;
+  border-radius: 3px;
+  font-size: 12px;
+  font-weight: normal;
+`;
+
+const ListItem = styled.li`
+  position: relative;
+  background-color: ${(props) => props.backgroundColor};
+  padding: 10px;
+  margin-bottom: 10px;
   border-radius: 5px;
-  font-size: 14px;
-  font-weight: bold;
 `;
