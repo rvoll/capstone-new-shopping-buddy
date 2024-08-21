b05ac655986bdeb97a673b13713bb86775ea790f
Rebecca Voll <doktavoll@gmail.com>
2024-08-21 12:10:32 +0200

    separate processing from rendering logic in index.js

--- pages/index.js
+++ pages/index.js
@@ -4,23 +4,30 @@ import { categories } from "../lib/categoriesData.js";
 import styled from "styled-components";
 
 export default function ShoppingItemsList() {
+  const categoryColorShoppingItems = shoppingItems.map((shoppingItem) => {
+    const category = categories.find(
+      (category) => category.name === shoppingItem.category
+    );
+    const backgroundColor = category ? category.color : "white";
+    return {
+      ...shoppingItem,
+      backgroundColor,
+    };
+  });
+
   return (
     <main>
       <StyledH1> {shoppingItems.length} Shopping Items </StyledH1>
       <StyledList>
-        {shoppingItems.map((shoppingItem) => {
-          const category = categories.find(
-            (category) => category.name === shoppingItem.category
-          );
-          const backgroundColor = category ? category.color : "white";
-
-          return (
-            <ListItem key={shoppingItem.id} $backgroundColor={backgroundColor}>
-              {shoppingItem.quantity} {shoppingItem.name}
-              <CategoryBox>{shoppingItem.category}</CategoryBox>
-            </ListItem>
-          );
-        })}
+        {categoryColorShoppingItems.map((shoppingItem) => (
+          <ListItem
+            key={shoppingItem.id}
+            $backgroundColor={shoppingItem.backgroundColor}
+          >
+            {shoppingItem.quantity} {shoppingItem.name}
+            <CategoryBox>{shoppingItem.category}</CategoryBox>
+          </ListItem>
+        ))}
       </StyledList>
     </main>
   );
